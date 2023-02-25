1e260a90b861beff7beb409c4a3a5abfbdc39110
adoctors <qinkeliang@rcrai.com>
2023-02-25 14:48:22 +0800

    🍱asset(kcm): 脚手架klc中git commit 格式化 kcm 的效果图

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# htmls
-static HTML pages
+静态资源
--- dist/page.head.script.js
+++
@@ -1,5 +0,0 @@
-var link = document.createElement('link');
-link.rel = "icon";
-link.type = "image/x-icon";
-link.href = '/favicon.png';
-document.head.appendChild(link);
