66d51f08c1f61428fd7290067b8883f7080f40c8
kl <qkeliang@163.com>
2024-05-19 11:08:48 +0800

    del demos exportStatic:true

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from "dumi";
 
+const isProd = process.env.NODE_ENV === "production";
+
+const config = isProd
+  ? {
+      base: "/htmls/",
+      publicPath: "/htmls/",
+      // exportStatic: false,
+      // history: { type: "hash" },
+    }
+  : {};
+
 export default defineConfig({
   themeConfig: {
     name: "kl_doc",
   },
-  exportStatic: false,
-  base: "/htmls",
-  publicPath: "/htmls/",
-  history: { type: "hash" },
+  ...config,
 });
